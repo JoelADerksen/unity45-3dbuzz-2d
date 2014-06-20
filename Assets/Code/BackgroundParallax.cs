818b9915653e387dafb33dadcbf9d41cd2174c4d
Joel Derksen <zedd.zorander@gmail.com>
2014-06-19 20:16:31 -0700

    Added object for background parallax scrolling

---
+++ Assets/Code/BackgroundParallax.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BackgroundParallax : MonoBehaviour {
+  public Transform[] Backgrounds;
+  public float ParallaxScale;
+  public float ParallaxReductionFactor;
+  public float Smoothing;
+
+  private Vector3 _lastPosition;
+
+  public void Start() {
+    _lastPosition = transform.position;
+  }
+
+  public void Update() {
+    var parallax = (_lastPosition.x - transform.position.x) * ParallaxScale;
+
+    for(var i = 0; i < Backgrounds.Length; ++i) {
+      var backgroundTargetPosition = Backgrounds[i].position.x + parallax * (i * ParallaxReductionFactor + 1);
+      Backgrounds[i].position = Vector3.Lerp(
+        Backgrounds[i].position,
+        new Vector3(backgroundTargetPosition, Backgrounds[i].position.y, Backgrounds[i].position.z),
+        Smoothing * Time.deltaTime);
+    }
+
+    _lastPosition = transform.position;
+  }
+}
