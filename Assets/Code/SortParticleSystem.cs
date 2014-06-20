6bfaf98bb3c5596acae98f75a61e1ccb98dceef8
Joel Derksen <zedd.zorander@gmail.com>
2014-06-19 21:25:44 -0700

    Added sorting system for particle systems

---
+++ Assets/Code/SortParticleSystem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+class SortParticleSystem : MonoBehaviour {
+  public string LayerName = "Particles";
+
+  public void Start() {
+    particleSystem.renderer.sortingLayerName = LayerName;
+  }
+}
